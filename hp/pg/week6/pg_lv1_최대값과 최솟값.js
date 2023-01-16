f94e7a8603eae2c82bb28cb724c80510353826ab
hovelopin <kimhojin3714@naver.com>
2023-01-16 22:29:34 +0900

    파일명 변경

--- hp/pg/week6/pg_lv1_최대값과 최솟값.js
+++
@@ -1,9 +0,0 @@
-// 시작 (12시 10분) => 끝 (12시 15분)
-function solution(nums) {
-  const numsArr = nums.split(" ").map((num) => +num);
-  const MAX_NUM = Math.max(...numsArr);
-  const MIN_NUM = Math.min(...numsArr);
-  return `${MIN_NUM} ${MAX_NUM}`;
-}
-
-console.log(solution("-1 -1"));
